a4781c9b12cdd06d925705c67b7ff16fa4893071
mng-g <mingazzini.michael@gmail.com>
2025-04-08 17:11:44 +0200

    changed az from us to eu

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -15,7 +15,7 @@ module "vpc" {
   source              = "../../modules/vpc"
   vpc_cidr            = "10.1.0.0/16"  # Use a CIDR that suits production
   vpc_name            = "prod-vpc"
-  availability_zones  = ["us-east-1a", "us-east-1b"] # Adjust based on your needs
+  availability_zones  = ["eu-north-1a", "eu-north-1b"] # Adjust based on your needs
   public_subnet_cidrs = ["10.1.1.0/24", "10.1.2.0/24"]
   private_subnet_cidrs = ["10.1.3.0/24", "10.1.4.0/24"]
   tags                = { Environment = "prod" }
