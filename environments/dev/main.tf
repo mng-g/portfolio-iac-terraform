fd0b287d26ead891af4542d213e2111aa4da045d
mng-g <mingazzini.michael@gmail.com>
2025-04-09 09:28:23 +0200

    set ami and changed instance_type to t3

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,16 +11,6 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
 module "vpc" {
   source              = "../../modules/vpc"
   vpc_cidr            = "10.0.0.0/16"
@@ -33,8 +23,8 @@ module "vpc" {
 
 module "ec2" {
   source         = "../../modules/ec2"
-  ami_id         = data.aws_ami.amazon_linux_2.id  # Use an appropriate free-tier eligible AMI (e.g., Amazon Linux 2)
-  instance_type  = "t2.micro"
+  ami_id         = "ami-0a2370e7c0f21e179" # Use an appropriate free-tier eligible AMI (e.g., Amazon Linux 2)
+  instance_type  = "t3.micro"
   subnet_id      = module.vpc.public_subnets[0]
   instance_name  = "dev-ec2-instance"
   tags           = { Environment = "dev" }
